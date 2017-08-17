d311c5d3b9fac4c8f35f10ab2f1fb72d9aa5fcae
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2017-08-17 19:52:30 -0400

    Implement drag-and-drop example

--- Examples/PDColorReceiverExample/ViewController.swift
+++ Examples/PDColorReceiverExample/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import PDColorPicker
 
 class ViewController: UIViewController {
 
+  // MARK: - Subviews
+
+  lazy var colorLabel: UILabel = {
+    let label = UILabel()
+
+    label.font = UIFont.systemFont(ofSize: 72, weight: UIFontWeightLight)
+    label.adjustsFontSizeToFitWidth = true
+    label.minimumScaleFactor = 0.25
+    label.text = "Drag a color here!"
+
+    return label
+  }()
+
+  // MARK: - Life Cycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    view.backgroundColor = .white
+
+    let dropInteraction = UIDropInteraction(delegate: self)
+    view.addInteraction(dropInteraction)
+
+    setupViews()
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+  private func setupViews() {
+    view.addSubview(colorLabel)
+    colorLabel.translatesAutoresizingMaskIntoConstraints = false
 
+    colorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    colorLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
 
 }
 
+// MARK: - UIDropInteraction
+extension ViewController: UIDropInteractionDelegate {
+  func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+    return session.canLoadObjects(ofClass: UIColor.self)
+  }
+
+  func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+    return UIDropProposal(operation: .copy)
+  }
+
+  func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+    session.loadObjects(ofClass: UIColor.self) {
+      guard let color = $0.first as? UIColor else { return }
+      let pdColor = PDColor(color: color)
+
+      self.view.backgroundColor = color
+      self.colorLabel.text = pdColor.hex
+      self.colorLabel.textColor = pdColor.appropriateForegroundColor
+    }
+  }
+}
