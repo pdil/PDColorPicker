78d9e2665d7b15baea5fd36839e141c651ed3f6e
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2017-08-17 19:50:53 -0400

    Fix filename references

--- Examples/Pods/Target Support Files/Pods-PDColorPickerExample/Pods-PDColorPickerExample-umbrella.h
+++ Examples/Pods/Target Support Files/Pods-PDColorPickerExample/Pods-PDColorPickerExample-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_PDColorPicker_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PDColorPicker_TestsVersionString[];
+FOUNDATION_EXPORT double Pods_PDColorPickerExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PDColorPickerExampleVersionString[];
 
--- Examples/Pods/Target Support Files/Pods-PDColorReceiverExample/Pods-PDColorReceiverExample-umbrella.h
+++ Examples/Pods/Target Support Files/Pods-PDColorReceiverExample/Pods-PDColorReceiverExample-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_PDColorPicker_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PDColorPicker_ExampleVersionString[];
+FOUNDATION_EXPORT double Pods_PDColorReceiverExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PDColorReceiverExampleVersionString[];
 
