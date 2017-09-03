3459a18aac319a636c3aa97514ddcf66b273643b
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2017-09-03 00:25:38 -0400

    Fix slider circle offset bug
    Fixed issue by making sure view is only laid out once in view controller. This issue has existed since v0.3.0.

--- PDColorPicker/Classes/PDColorPickerViewController.swift
+++ PDColorPicker/Classes/PDColorPickerViewController.swift
@@ -181,28 +181,32 @@ open class PDColorPickerViewController: UIViewController {
     }
   }
 
+  var constraintsHaveBeenSet = false
   override open func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
 
-    guard let pvc = presentingViewController else { return }
+    if !constraintsHaveBeenSet {
+      guard let pvc = presentingViewController else { return }
 
-    let aspectRatio: CGFloat = 0.7
+      let aspectRatio: CGFloat = 0.7
 
-    view.frame.size.height = pvc.view.frame.height * 0.7
-    view.frame.size.width = min(view.frame.height * aspectRatio, pvc.view.frame.width * 0.9)
+      view.frame.size.height = pvc.view.frame.height * 0.7
+      view.frame.size.width = min(view.frame.height * aspectRatio, pvc.view.frame.width * 0.9)
 
-    view.frame.origin.x = pvc.view.frame.width / 2 - view.frame.width / 2
-    view.frame.origin.y = pvc.view.frame.height / 2 - view.frame.height / 2
+      view.frame.origin.x = pvc.view.frame.width / 2 - view.frame.width / 2
+      view.frame.origin.y = pvc.view.frame.height / 2 - view.frame.height / 2
 
-    view.layoutIfNeeded()
+      view.layoutIfNeeded()
 
-    selectedColorLabel.layer.cornerRadius = selectedColorLabel.frame.height / 2
+      selectedColorLabel.layer.cornerRadius = selectedColorLabel.frame.height / 2
 
-    colorPickerView.currentColor = currentColor
-    colorSliderView.currentHue = currentColor.h
-    colorPickerView.fadeSlider(.in)
+      colorPickerView.currentColor = currentColor
+      colorSliderView.currentHue = currentColor.h
+      colorPickerView.fadeSlider(.in)
 
-    colorChanged(to: currentColor)
+      colorChanged(to: currentColor)
+      constraintsHaveBeenSet = true
+    }
   }
 
   // MARK: - Button Targets
